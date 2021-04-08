e03d3f3b981f0f4715aefe3972c640f9b910d624
sandeep23097 <sandeepsinghmbbs@gmail.com>
2021-04-07 23:53:12 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,72 +1,42 @@
-const HTTPS_PORT = 8443; //default port for https is 443
-const HTTP_PORT = 8001; //default port for http is 80
 
-const fs = require('fs');
-const http = require('http');
-const https = require('https');
-const WebSocket = require('ws');
-// based on examples at https://www.npmjs.com/package/ws 
-const WebSocketServer = WebSocket.Server;
+const server = require('http').createServer()
+const io = require('socket.io')(server)
 
-// Yes, TLS is required
-const serverConfig = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem'),
-};
+io.on('connection', function (client) {
 
-// ----------------------------------------------------------------------------------------
+  console.log('client connect...', client.id);
 
-// Create a server for the client html page
-const handleRequest = function (request, response) {
-  // Render the single client html file for any request the HTTP server receives
-  console.log('request received: ' + request.url);
+  client.on('typing', function name(data) {
+    console.log(data);
+    io.emit('typing', data)
+  })
 
- if (request.url === '/webrtc.js') {
-    response.writeHead(200, { 'Content-Type': 'application/javascript' });
-    response.end(fs.readFileSync('client/webrtc.js'));
-  } else if (request.url === '/style.css') {
-    response.writeHead(200, { 'Content-Type': 'text/css' });
-    response.end(fs.readFileSync('client/style.css'));
-  } else {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.end(fs.readFileSync('client/index.html'));
-  }
-};
+  client.on('message', function name(data) {
+    console.log(data);
+    io.emit('message', data)
+  })
 
-const httpsServer = https.createServer(serverConfig, handleRequest);
-httpsServer.listen(HTTPS_PORT);
+  client.on('location', function name(data) {
+    console.log(data);
+    io.emit('location', data);
+  })
 
-// ----------------------------------------------------------------------------------------
+  client.on('connect', function () {
+  })
 
-// Create a server for handling websocket calls
-const wss = new WebSocketServer({ server: httpsServer });
+  client.on('disconnect', function () {
+    console.log('client disconnect...', client.id)
+    // handleDisconnect()
+  })
 
-wss.on('connection', function (ws) {
-  ws.on('message', function (message) {
-    // Broadcast any received message to all clients
-    console.log('received: %s', message);
-    wss.broadcast(message);
-  });
+  client.on('error', function (err) {
+    console.log('received error from client:', client.id)
+    console.log(err)
+  })
+})
 
-  ws.on('error', () => ws.terminate());
+var server_port = process.env.PORT || 3000;
+server.listen(server_port, function (err) {
+  if (err) throw err
+  console.log('Listening on port %d', server_port);
 });
-
-wss.broadcast = function (data) {
-  this.clients.forEach(function (client) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(data);
-    }
-  });
-};
-
-console.log('Server running.'
-);
-
-// ----------------------------------------------------------------------------------------
-
-// Separate server to redirect from http to https
-http.createServer(function (req, res) {
-    console.log(req.headers['host']+req.url);
-    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-    res.end();
-}).listen(HTTP_PORT);
